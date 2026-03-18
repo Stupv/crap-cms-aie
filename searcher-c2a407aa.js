5c182d4be388bc63ebd2c0013bb6a59a2bb1097d
dkluhzeb <dkluhzeb@users.noreply.github.com>
2026-03-18 15:24:17 +0000

    deploy: b065d27c2f6dceae062faaef03ad647b1298d03e

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-95b5fa60.js',
+                path_to_root + 'searchindex-0d4ad855.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
